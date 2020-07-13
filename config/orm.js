3e659447500176e10d422932d694e94e01076d5e
Sam Dratch <dratch.sam1@gmail.com>
2020-07-13 17:52:40 -0400

    base layout for orm file

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("../config/connection.js");
+
+const orm = {
+
+    selectall:function(){
+
+    },
+    insertOne:function(){
+
+    },
+    updateOne:function(){
+
+    }
+
+}
