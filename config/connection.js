5e01a24643b81a0bc3f85ee827b093e5bb531d8d
Sam Dratch <dratch.sam1@gmail.com>
2020-07-14 12:11:50 -0400

    fixed minor errors

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+
 let connection;
 if (process.env.JAWSDB_URL) {
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
    connection = mysql.createConnection({
     host: "localhost",
