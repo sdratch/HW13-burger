df0f484633985eea29417f3f98719e4e356795e2
Sam Dratch <dratch.sam1@gmail.com>
2020-07-14 12:48:15 -0400

    making heroku work 3

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-// let connection;
-// if (process.env.JAWSDB_URL) {
-//   var connection = mysql.createConnection(process.env.JAWSDB_URL);
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//    connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "poiuy123",
-//     database: "burgers_db",
-//   });
-// }
-let connection = mysql.createConnection(process.env.JAWSDB_URL);
+let connection;
+if (process.env.JAWSDB_URL) {  
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log(connection)
+} else {
+   connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "poiuy123",
+    database: "burgers_db",
+  });
+}
+
 // Make connection.
 connection.connect((err) => {
   if (err) {
