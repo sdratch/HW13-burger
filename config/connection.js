44359c02f6656edb50095c2770007d3930aacfa9
Sam Dratch <dratch.sam1@gmail.com>
2020-07-14 12:57:56 -0400

    making heroku work 4

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 
 let connection;
 if (process.env.JAWSDB_URL) {  
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
   console.log(connection)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
    connection = mysql.createConnection({
     host: "localhost",
